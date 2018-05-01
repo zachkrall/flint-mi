ccc5721daff98a8c597a7417abb318c03d856af7
Zach Krall <zach@zachkrall.com>
2018-05-01 09:19:21 -0400

    Create local_settings_example.py

---
+++ local_settings_example.py
@@ -0,0 +1,11 @@
+'''
+Local Settings
+'''
+
+MY_CONSUMER_KEY = 'Your Twitter API Consumer Key'
+MY_CONSUMER_SECRET = 'Your Consumer Secret Key'
+MY_ACCESS_TOKEN_KEY = 'Your Twitter API Access Token Key'
+MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
+
+DEBUG = True
+TWEET_ACCOUNT = ""
